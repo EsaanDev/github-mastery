256368371c184380f12ce2f60fff12548b8dbe30
EsaanDev <esaansk2060@gmail.com>
2025-05-28 12:41:15 +0545

    button function created

--- test.js
+++ test.js
@@ -3,3 +3,12 @@ console.log("Firat commit")
 function findUser(user1, user2) {
     console.log(user1, user2)
 }
+
+function developers() {
+    console.log("Saiman and Keshab");
+}
+
+
+function button() {
+    console.log("Clicked button");
+}

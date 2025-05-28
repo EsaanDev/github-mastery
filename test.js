d2fba200cba26f58975649eaa05888c5f5eb031a
EsaanDev <esaansk2060@gmail.com>
2025-05-28 12:01:38 +0545

    signup function added

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ console.log("Firat commit")
 function findUser(user1, user2) {
     console.log(user1, user2)
 }
+
+function signup(username, email, password) {
+    console.log(`Fullname:${username}, Email:${email}, password: ${password}`)
+}
